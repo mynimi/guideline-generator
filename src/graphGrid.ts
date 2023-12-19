c40ac4184697f674a7d65ff04284bd0e65ba51fd
Myriam <myriam@halfapx.com>
2023-12-19 11:45:14 +0100

    graphGridOptimization

--- src/graphGrid.ts
+++ src/graphGrid.ts
@@ -26,21 +26,21 @@ export interface GraphGridTechOptions {
   textLineHeight?: number;
 }
 
-type RequiredFields<T> = Required<{ [K in keyof T]: T[K] }>;
+type RequiredFields<T> = {[K in keyof T]-?: T[K];};
 
 export type GraphGridConfig = GraphGridBasicOptions & GraphGridExtendedOptions & GraphGridTechOptions;
 
 export class GraphGrid {
   #defaults: RequiredFields<GraphGridConfig>;
   #config: RequiredFields<GraphGridConfig>;
-  #defaultStrokeSize: number;
-  #defaultStrokeColor: string;
-  #copyrightSizeFactor: number;
-  #textBuffer: number;
-  #fontColor: string;
-  #copyRightText: string;
   #svg: SVGElement;
   #maskId: string;
+  readonly #defaultStrokeSize: number = 0.2;
+  readonly #defaultStrokeColor: string = "#000000";
+  readonly #copyrightSizeFactor: number = 0.7;
+  readonly #textBuffer: number = 2;
+  readonly #fontColor: string = "#808080";
+  readonly #copyRightText: string = "© grid code.halfapx.com/guideline-generator/";
 
   get defaultValues(): RequiredFields<GraphGridConfig> {
     return this.#defaults;
@@ -51,53 +51,60 @@ export class GraphGrid {
   }
 
   get width(): number {
-    return this.#config.documentWidth;
+    return this.#config.documentWidth!;
   }
 
   get height(): number {
-    return this.#config.documentHeight;
+    return this.#config.documentHeight!;
   }
 
   private get textHeight(): number {
-    return this.#config.textFontSize * this.#config.textLineHeight + this.#textBuffer;
+    return (
+      (this.#config.textFontSize! * this.#config.textLineHeight!) +
+      this.#textBuffer
+    );
   }
 
   private get marginTop(): number {
-    return this.#config.addTitle ? this.#config.documentMarginTop + this.textHeight : this.#config.documentMarginTop;
+    return this.#config.addTitle!
+      ? this.#config.documentMarginTop! + this.textHeight
+      : this.#config.documentMarginTop!;
   }
 
   private get marginBottom(): number {
-    return this.#config.documentMarginBottom + this.textHeight * this.#copyrightSizeFactor;
+    return (
+      this.#config.documentMarginBottom! +
+      this.textHeight * this.#copyrightSizeFactor
+    );
   }
 
-  private get marginLeft():number {
-    return this.#config.documentMarginLeft;
+  private get marginLeft(): number {
+    return this.#config.documentMarginLeft!;
   }
 
-  private get marginRight():number {
-    return this.#config.documentMarginRight;
+  private get marginRight(): number {
+    return this.#config.documentMarginRight!;
   }
 
-  private get gridWidth():number {
-    return this.width - this.#config.documentMarginLeft - this.#config.documentMarginRight;
+  private get gridWidth(): number {
+    return (
+      this.width - this.#config.documentMarginLeft! - this.#config.documentMarginRight!
+    );
   }
 
-  private get gridHeight():number {
+  private get gridHeight(): number {
     return this.height - this.marginTop - this.marginBottom;
   }
 
-  get prettyName():string {
+  get prettyName(): string {
     return this.generateGridName("pretty");
   }
 
-  get fileName():string {
+  get fileName(): string {
     return this.generateGridName("file");
   }
 
-  constructor(options?: GraphGridConfig) {
-    this.#defaultStrokeColor = "#000000";
-    this.#defaultStrokeSize = 0.2;
-
+  constructor(options: Partial<GraphGridConfig> = {}) {
     this.#defaults = {
       documentWidth: 210,
       documentHeight: 297,
@@ -112,95 +119,103 @@ export class GraphGrid {
       areaStrokeWidth: this.#defaultStrokeSize,
       lineColor: this.#defaultStrokeColor,
       areaStrokeColor: this.#defaultStrokeColor,
-      container: document.querySelector("[data-svg-preview]"),
+      container: document.querySelector("[data-svg-preview]")!,
       coordinateDecimalPlaceMax: 2,
       textFontSize: 4,
       textLineHeight: 1.2,
       addTitle: true,
-      gridType: "graph",
+      gridType: "graph"
     };
 
-    this.#config = options ? { ...this.#defaults, ...options } : this.#defaults;
+    this.#config = {...this.#defaults, ...options};
 
-    this.#fontColor = "#808080";
-    this.#copyrightSizeFactor = 0.7;
-    this.#textBuffer = 2;
-    this.#copyRightText = "© grid code.halfapx.com/guideline-generator/";
     this.init();
   }
 
-  init() {
+  init(): void {
     this.#svg = this.createDocument();
     this.addTitleAndCopyright();
 
     if (this.#config.addAreaBox) {
       this.#maskId = "grid-mask";
       this.drawMask();
-      this.drawRectangle(this.#svg, "transparent", this.#config.areaStrokeColor);
+      this.drawRectangle(this.#svg,"transparent",this.#config.areaStrokeColor!);
     }
     this.drawGraphGrid();
     this.renderSVG();
   }
 
   renderSVG(): void {
-    this.#config.container.appendChild(this.#svg);
+    this.#config.container!.appendChild(this.#svg);
   }
 
   removeSVG(): void {
-    this.#config.container.removeChild(this.#svg);
+    this.#config.container!.removeChild(this.#svg);
   }
 
   private addTitleAndCopyright(): void {
-    const copyrightFontSize = this.#config.textFontSize * this.#copyrightSizeFactor;
-    const copyrightTopPos = this.height - this.#config.documentMarginBottom;
-    const copyrightLeftPos = this.#config.documentMarginLeft;
+    const copyrightFontSize = this.#config.textFontSize! * this.#copyrightSizeFactor;
+    const copyrightTopPos = this.height - this.#config.documentMarginBottom!;
+    const copyrightLeftPos = this.#config.documentMarginLeft!;
 
-    this.addTextString(this.#svg, this.#copyRightText, copyrightFontSize, "start", copyrightTopPos, copyrightLeftPos);
+    this.addTextString(
+      this.#svg,
+      this.#copyRightText,
+      copyrightFontSize,
+      "start",
+      copyrightTopPos,
+      copyrightLeftPos
+    );
 
     if (this.#config.addTitle) {
-      const titleFontSize = this.#config.textFontSize;
-      const titleTopPos = titleFontSize * this.#config.textLineHeight + this.#config.documentMarginTop;
-      const titleLeftPos = this.#config.documentWidth - this.#config.documentMarginRight;
+      const titleFontSize = this.#config.textFontSize!;
+      const titleTopPos = titleFontSize * this.#config.textLineHeight! + this.#config.documentMarginTop!;
+      const titleLeftPos = this.#config.documentWidth! - this.#config.documentMarginRight!;
 
-      this.addTextString(this.#svg, this.prettyName, titleFontSize, "end", titleTopPos, titleLeftPos);
+      this.addTextString(
+        this.#svg,
+        this.prettyName,
+        titleFontSize,
+        "end",
+        titleTopPos,
+        titleLeftPos
+      );
     }
   }
 
   private generateGridName(type: "pretty" | "file"): string {
-    const separator = type == "pretty" ? " " : "_";
-    const name = this.#config.gridType == "dot" ? "dot grid" : "graph";
+    const separator = type === "pretty" ? " " : "_";
+    const name = this.#config.gridType === "dot" ? "dot grid" : "graph";
     return `${name}${separator}${this.#config.cellSize}mm`;
   }
 
   private createDocument(): SVGElement {
-    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-
+    const svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
     const viewBox = `0 0 ${this.width} ${this.height}`;
     svg.setAttribute("viewBox", viewBox);
-
     return svg;
   }
 
-  private drawGraphGrid() {
-    const cellSize = this.#config.cellSize;
-    const gridType = this.#config.gridType;
+  private drawGraphGrid(): void {
+    const cellSize = this.#config.cellSize!;
+    const gridType = this.#config.gridType!;
     const xEnd = this.width - this.marginRight;
     const yEnd = this.height - this.marginBottom;
-    const horizontalIntersections = []; // used to generate dots
+    const horizontalIntersections: number[] = [];
     const horizontalReps = this.gridHeight / cellSize;
     const horizontalRemainder = this.gridHeight % cellSize;
-    const verticalIntersections = []; // used to generate dots
+    const verticalIntersections: number[] = [];
     const verticalReps = this.gridWidth / cellSize;
     const verticalRemainder = this.gridWidth % cellSize;
 
-    const gridParent = this.createGroup("grid", "calli-grid", this.#maskId ? this.#maskId : undefined);
+    const gridParent = this.createGroup("grid","calli-grid",this.#maskId ? this.#maskId : undefined);
 
     let yLineStart = this.marginTop + horizontalRemainder / 2;
     for (let i = 0; i <= horizontalReps; i++) {
-      if (gridType == "graph") {
-        this.drawLine(gridParent, 'horizontal', yLineStart, this.marginLeft, xEnd);
+      if (gridType === "graph") {
+        this.drawLine(gridParent, "horizontal", yLineStart, this.marginLeft, xEnd);
       }
-      if (gridType == "dot") {
+      if (gridType === "dot") {
         horizontalIntersections.push(yLineStart);
       }
       yLineStart += cellSize;
@@ -208,57 +223,57 @@ export class GraphGrid {
 
     let xLineStart = this.marginLeft + verticalRemainder / 2;
     for (let i = 0; i <= verticalReps; i++) {
-      if (gridType == "graph") {
-        this.drawLine(gridParent, 'vertical', xLineStart, this.marginTop, yEnd);
+      if (gridType === "graph") {
+        this.drawLine(gridParent, "vertical", xLineStart, this.marginTop, yEnd);
       }
-      if (gridType == "dot") {
+      if (gridType === "dot") {
         verticalIntersections.push(xLineStart);
       }
       xLineStart += cellSize;
     }
 
-    if (gridType == "dot") {
+    if (gridType === "dot") {
       this.drawDotsAtPoints(gridParent, horizontalIntersections, verticalIntersections);
     }
 
     this.#svg.appendChild(gridParent);
   }
 
-  private drawDotsAtPoints(parentEl: SVGElement, horizontalPoints: number[], verticalPoints: number[]): void {
+  private drawDotsAtPoints(parentEl: SVGElement,horizontalPoints: number[],verticalPoints: number[]): void {
     for (const horizontalPoint of horizontalPoints) {
       for (const verticalPoint of verticalPoints) {
-        const dot = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        dot.setAttribute("cx", this.formatCoordinate(verticalPoint).toString());
-        dot.setAttribute("cy", this.formatCoordinate(horizontalPoint).toString());
-        dot.setAttribute("r", this.#config.gridStrokeWidth.toString()); // Adjust the dot size as needed
-        dot.setAttribute("fill", this.#config.lineColor); // Adjust the dot color as needed
+        const dot = document.createElementNS("http://www.w3.org/2000/svg","circle");
+        dot.setAttribute("cx",this.formatCoordinate(verticalPoint).toString());
+        dot.setAttribute("cy",this.formatCoordinate(horizontalPoint).toString());
+        dot.setAttribute("r",this.#config.gridStrokeWidth!.toString());
+        dot.setAttribute("fill",this.#config.lineColor!);
         parentEl.appendChild(dot);
       }
     }
   }
 
-  private drawRectangle(parentEl, fillColor, strokeColor) {
-    const rectangle = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  private drawRectangle(parentEl: SVGElement,fillColor: string,strokeColor: string): void {
+    const rectangle = document.createElementNS("http://www.w3.org/2000/svg","rect");
     rectangle.setAttribute("x", this.marginLeft.toString());
     rectangle.setAttribute("y", this.marginTop.toString());
     rectangle.setAttribute("width", this.gridWidth.toString());
     rectangle.setAttribute("height", this.gridHeight.toString());
-    rectangle.setAttribute("rx", this.#config.areaBorderRadius.toString());
-    rectangle.setAttribute("fill", fillColor);
-    rectangle.setAttribute("stroke-width", this.#config.areaStrokeWidth.toString());
-    rectangle.setAttribute("stroke", strokeColor);
+    rectangle.setAttribute("rx",this.#config.areaBorderRadius!.toString());
+    rectangle.setAttribute("fill",fillColor);
+    rectangle.setAttribute("stroke-width",this.#config.areaStrokeWidth!.toString());
+    rectangle.setAttribute("stroke",strokeColor);
     parentEl.appendChild(rectangle);
   }
 
-  private drawMask():void {
-    const mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
-    mask.setAttribute("id", this.#maskId);
+  private drawMask(): void {
+    const mask = document.createElementNS("http://www.w3.org/2000/svg","mask");
+    mask.setAttribute("id", this.#maskId!);
     this.drawRectangle(mask, "white", "black");
     this.#svg.appendChild(mask);
   }
 
-  private createGroup(className?: string, idName?: string, maskId?: string): SVGElement {
-    const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  private createGroup(className?: string,idName?: string,maskId?: string): SVGElement {
+    const group = document.createElementNS("http://www.w3.org/2000/svg","g");
     if (className) {
       group.setAttribute("class", className);
     }
@@ -271,44 +286,41 @@ export class GraphGrid {
     return group;
   }
 
-  private drawLine(parentEl:SVGElement, orientation: 'horizontal' | 'vertical', gridPos:number, lineStart:number, lineEnd:number,){
+  private drawLine(parentEl: SVGElement,orientation: "horizontal" | "vertical",gridPos: number,lineStart: number,lineEnd: number): void {
     let x1, x2, y1, y2;
-    if (orientation === 'horizontal') {
+    if (orientation === "horizontal") {
       x1 = lineStart;
       x2 = lineEnd;
       y1 = y2 = gridPos;
     }
-  
-    if (orientation === 'vertical') {
+    if (orientation === "vertical") {
       y1 = lineStart;
       y2 = lineEnd;
       x1 = x2 = gridPos;
     }
-    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    const line = document.createElementNS("http://www.w3.org/2000/svg","line");
     line.setAttribute("x1", this.formatCoordinate(x1).toString());
     line.setAttribute("x2", this.formatCoordinate(x2).toString());
     line.setAttribute("y1", this.formatCoordinate(y1).toString());
     line.setAttribute("y2", this.formatCoordinate(y2).toString());
-    line.setAttribute("stroke", this.#config.lineColor);
-    line.setAttribute("stroke-width", this.#config.gridStrokeWidth.toString());
-
+    line.setAttribute("stroke", this.#config.lineColor!);
+    line.setAttribute("stroke-width",this.#config.gridStrokeWidth!.toString());
     parentEl.appendChild(line);
   }
 
   private addTextString(parentEl: SVGElement,text: string,fontSize: number,textAnchor: "start" | "end",topPos: number,leftPos: number): void {
-    const textString = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    const textString = document.createElementNS("http://www.w3.org/2000/svg","text");
     textString.textContent = text;
     textString.setAttribute("x", leftPos.toString());
     textString.setAttribute("y", topPos.toString());
     textString.setAttribute("text-anchor", textAnchor);
     textString.setAttribute("font-size", fontSize.toString());
     textString.setAttribute("fill", this.#fontColor);
-    textString.setAttribute("font-family", "Arial, sans-serif"); // Web-safe font
-
+    textString.setAttribute("font-family", "Arial, sans-serif");
     parentEl.appendChild(textString);
   }
 
   private formatCoordinate(n: number): string {
-    return parseFloat(n.toFixed(this.#config.coordinateDecimalPlaceMax)).toString();
+    return parseFloat(n.toFixed(this.#config.coordinateDecimalPlaceMax!)).toString();
   }
 }
