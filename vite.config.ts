778818700d080937110b038bdea00c703c0e6163
Myriam <myriam@halfapx.com>
2023-12-20 18:02:10 +0100

    remove non-necessary files

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        all: resolve(__dirname, 'all/index.html'),
+        // all: resolve(__dirname, 'all/index.html'),
       },
     },
   },
