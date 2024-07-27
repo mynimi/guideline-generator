2617c50e165e0aca0bc578de489159d09d4175a7
Myriam <myriam@halfapx.com>
2024-07-27 17:47:38 +0200

    build adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://code.halfapx.com/guideline-generator/",
+  buildOptions: {
+    site: "https://code.halfapx.com/guideline-generator/",
+  },
   experimental: {
     contentCollectionCache: false,
   },
