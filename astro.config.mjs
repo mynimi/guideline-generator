a4e9931c61e47a907c1da9dce72cea1b130ad50d
Myriam <myriam@halfapx.com>
2024-07-27 17:41:50 +0200

    add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://code.halfapx.com/guideline-generator/",
   experimental: {
     contentCollectionCache: false,
   },
