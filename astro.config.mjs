37aa4a97cd59f414fb49e205373276b446060a7a
Myriam <myriam@halfapx.com>
2024-07-27 17:58:29 +0200

    build with relative links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://code.halfapx.com",
   base: "/guideline-generator/",
   experimental: {
-    contentCollectionCache: false,
+    contentCollectionCache: false
   },
+  integrations: [relativeLinks()]
 });
