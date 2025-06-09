f8365b412e9a4308ad0dc8b6d84c2bae15d2c664
Myriam <myriam@halfapx.com>
2025-06-09 14:47:04 +0200

    update to calligraphy-grids-v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,4 @@ export default defineConfig({
   site: "https://code.halfapx.com",
   base: "guideline-generator",
   trailingSlash: "ignore",
-  experimental: {
-    contentCollectionCache: false
-  }
 });
